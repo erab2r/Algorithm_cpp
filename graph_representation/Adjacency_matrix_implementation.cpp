ce3f4182226cab6d38abebbdd88a52ff3962da1e
erab2r <shkera213@gmail.com>
2025-08-18 03:17:49 +0600

    d

--- graph_representation/Adjacency_matrix_implementation.cpp
+++ graph_representation/Adjacency_matrix_implementation.cpp
@@ -5,10 +5,30 @@ int main()
     int n,e;
     cin >> n >> e;
     int adj_matrix [n][n];
-    for(int i = 0; i<n; i++)
+
+    // for(int i = 0;i<n;i++)
+    // {
+    //     for(int j = 0; j<n;j++)
+    //     {
+    //         adj_matrix[i][j] = 0; 
+    //     }
+    // }
+    memset(adj_matrix, 0, sizeof(adj_matrix));
+     for(int i = 0; i<e; i++)
     {
         int a,b;
         cin >> a >> b;
+        adj_matrix[a][b] = 1;
+        adj_matrix[b][a] = 1;
     }
+      for(int i = 0;i<n;i++)
+    {
+        for(int j = 0; j<n;j++)
+        {
+           cout << adj_matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+   
 
 }
