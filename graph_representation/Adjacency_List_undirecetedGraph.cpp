ed6cf1f93c2669e138540804ff1ddf2fc6f5e325
erab2r <shkera213@gmail.com>
2025-08-18 21:41:04 +0600

    1

---
+++ graph_representation/Adjacency_List_undirecetedGraph.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,e;
+    cin >> n >> e;
+    vector<int> adj_matrix[n];
+    for(int i = 0; i<e;i++)
+    {
+        int a,b;
+        cin >> a >> b;
+        adj_matrix[a].push_back(b);
+        adj_matrix[b].push_back(a);
+    }
+    for(int i = 0; i<n;i++)
+    {
+        cout << i << " -> ";
+        for(int x : adj_matrix[i])
+        {
+            cout << x << " ";       
+        }
+        cout << endl;
+    }
+    return  0;
+}
