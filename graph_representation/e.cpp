adf43395c6621c4cb999abde76a372b20659fbdb
erab2r <shkera213@gmail.com>
2025-08-19 21:46:59 +0600

    3

--- graph_representation/e.cpp
+++ graph_representation/e.cpp
@@ -4,29 +4,29 @@ int main()
 {
     int n,e;
     cin >> n >> e;
-    int a[n];
-    int adj_matrix [n][n];
+    int adj_matrix[n][n];
     memset(adj_matrix, 0, sizeof(adj_matrix));
     for(int i = 0;i<n;i++) 
-        for(int j = 0; j<n;j++)
-            if(i == j)
+        for(int j = 0; j<n;j++) 
+            if(i == j) 
                 adj_matrix[i][j] = 1;
-               
-     for(int i = 0; i<e; i++)
+
+    for(int i = 0; i < e; i++)
     {
         int a,b;
         cin >> a >> b;
+        a--; 
+        b--; 
         adj_matrix[a][b] = 1;
         adj_matrix[b][a] = 1;
     }
-      for(int i = 0;i<n;i++)
+
+    for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j<n;j++)
+        for(int j = 0; j < n; j++)
         {
-           cout << adj_matrix[i][j] << " ";
+            cout << adj_matrix[i][j] << " ";
         }
         cout << endl;
     }
-   
-
 }
---
+++ graph_representation/e2.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,e;
+    cin >> n >> e;
+    vector<int> adj_matrix[n];
+    for(int i = 0; i<e;i++)
+    {
+        int a,b;
+        cin >> a >> b;
+        a--;  
+        b--;  
+        adj_matrix[a].push_back(b);
+        adj_matrix[b].push_back(a);
+    }
+    for(int i = 0; i<n;i++)
+    {
+        cout << i+1 << " -> ";
+        for(int x : adj_matrix[i])
+        {
+            cout << x+1 << " ";       
+        }
+        cout << endl;
+    }
+    return  0;
+}
