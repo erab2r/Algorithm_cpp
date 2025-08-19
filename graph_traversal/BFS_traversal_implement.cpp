14721926d495219f846dab4a82ba0b250e0afbc1
erab2r <shkera213@gmail.com>
2025-08-19 17:18:44 +0600

    2

--- graph_traversal/BFS_traversal_implement.cpp
+++ graph_traversal/BFS_traversal_implement.cpp
@@ -1,6 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
+vector<int>adj_list[1005];
+bool visited[1005];
+void bfs(int src)
 {
-    return 0;
+    queue<int>q;
+    q.push(src);
+    visited[src] = true;
+    while (!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+        cout << par << " ";
+        for(int child : adj_list[par])
+        {
+            if(visited[child] == false)
+            {
+                visited[child] = true;
+                q.push(child);
+            }
+        }
+
+    }
+    
+
+}
+int main()
+{   
+    int n,e;
+    cin >> n >> e;
+    for(int i = 0; i<e;i++)
+    {
+        int a,b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a); 
+    }
+    bfs(0);
+     return 0;
 }
