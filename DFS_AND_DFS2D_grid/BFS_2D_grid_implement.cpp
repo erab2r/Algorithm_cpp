f9530b491d8911e3ba5242fb75d91f475621594f
erab2r <shkera213@gmail.com>
2025-08-23 00:49:09 +0600

    2

--- DFS_AND_DFS2D_grid/BFS_2D_grid_implement.cpp
+++ DFS_AND_DFS2D_grid/BFS_2D_grid_implement.cpp
@@ -1,51 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
 char grid[105][105];
-bool visited[105][105];
-int n,m;
+bool vis[105][105];
+int level[105][105];
 vector<pair<int,int>> d = {{-1,0},{1,0},{0,-1},{0,1}};
-bool valid(int i,int j)
+int n,m;
+
+bool valid(int i, int j)
 {
     if(i<0 || i>=n || j<0 || j>=m)
         return false;
     return true;
-    
 }
-void bfs(int si,int sj)
+
+void bfs(int si, int sj)
 {
     queue<pair<int,int>> q;
     q.push({si,sj});
-    visited[si][sj] = true;
-    while (!q.empty())
+    vis[si][sj] = true;
+    level[si][sj] = 0;
+    while(!q.empty())
     {
-        pair<int,int>  par  = q.front();
+        pair<int,int> par = q.front();
         q.pop();
         int par_i = par.first;
         int par_j = par.second;
-        cout << par_i << " " << par_j << endl;
-        for(int i = 0; i<4;i++)
+
+        for(int i=0;i<4;i++)
         {
             int ci = par_i + d[i].first;
             int cj = par_j + d[i].second;
-            if(valid(ci,cj)== true && !visited[ci][cj])
-            {    
+            if(valid(ci,cj) && !vis[ci][cj] && grid[ci][cj]=='.')
+            {
                 q.push({ci,cj});
-                visited[ci][cj] = true;
+                vis[ci][cj] = true;
+                level[ci][cj] = level[par_i][par_j] + 1;
+            }
         }
     }
-    
-   }
 }
+
 int main()
 {
     cin >> n >> m;
-    for(int i = 0;i<n;i++)
-        for(int j = 0;j<m;j++)
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
             cin >> grid[i][j];
-    int si,sj;
-    cin >> si >> sj;
-    memset(visited,false,sizeof(visited));
+
+    int si,sj,di,dj;
+    cin >> si >> sj >> di >> dj;
+    memset(vis,false,sizeof(vis));
+    memset(level,-1,sizeof(level));
     bfs(si,sj);
-    return 0; 
-    
+    cout << level[di][dj] << endl;
+    return 0;
 }
