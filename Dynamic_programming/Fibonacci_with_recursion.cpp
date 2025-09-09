105a93f4b2c854671e64b363ff1174b79a080a5b
erab2r <shkera213@gmail.com>
2025-09-10 03:26:03 +0600

    1

--- Dynamic_programming/Fibonacci_with_recursion.cpp
+++ Dynamic_programming/Fibonacci_with_recursion.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 int fibo(int n)
 {
-    if(n==1)
-        return 1;
-    if(n==0)
-        return 0;
+    // if(n==1)
+    //     return 1;
+    // if(n==0)
+    //     return 0;
+    if(n<2)
+        return n;
     return fibo(n-1) + fibo(n-2);
 }
 int main()
